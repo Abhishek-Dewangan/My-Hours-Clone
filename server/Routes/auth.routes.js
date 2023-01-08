70c1e8297492475c28b8c3e3bb6ba06bd04e74ae
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-01-08 22:50:03 +0530

    Server update

--- server/Routes/auth.routes.js
+++ server/Routes/auth.routes.js
@@ -1,56 +1,47 @@
-const {Router} = require("express");
-const UserModel = require("../Models/User.auth")
+const {Router} = require('express');
+const UserModel = require('../Models/User.auth');
 const authRouter = Router();
 
-authRouter.post("/signup", async(req,res)=>{
-    console.log(req.body);
-    const user = await new UserModel(req.body);
-    user.save((err,success)=>{
-        if(err){
-            res.status(500).send({message: "error occurred"})
-        }
-        return res.status(201).send({message: "Signed up successfully", token : 12345})
-    });
-    
-})
-
-authRouter.get("/user", async(req,res)=>{
-    // console.log(req.params.id,"17")
-
-    res.setHeader("content-type","application/json")
-
-    
-    try {
-  
-        const data =  await new UserModel.find({})
-        console.log(data)
-         return res.status(200).json(data)
-       }catch(error){
-         res.status(404).json(error) 
-       
-     }
-
-   
-
-    
-})
-
-
-
-authRouter.post("/signin", async(req,res)=>{
-    console.log(req.body);
-    const checkuser = await UserModel.find(req.body).lean();
-    if(checkuser.length>=1){
-        let payload = {
-            ...checkuser[0],
-            token : 12345
-        }
-        console.log(checkuser)
-        res.send(payload);
+authRouter.post('/signup', async (req, res) => {
+  console.log(req.body);
+  const user = await new UserModel(req.body);
+  user.save((err, success) => {
+    if (err) {
+      res.status(500).send({message: 'error occurred'});
     }
-    else{
-        res.send({message:"wrong credentials"})
-    }
+    return res
+      .status(201)
+      .send({message: 'Signed up successfully', token: 12345});
+  });
+});
+
+authRouter.get('/user', async (req, res) => {
+  // console.log(req.params.id,"17")
+
+  res.setHeader('content-type', 'application/json');
+
+  try {
+    const data = await new UserModel.find({});
+    console.log(data);
+    return res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+});
+
+authRouter.post('/signin', async (req, res) => {
+  console.log(req.body);
+  const checkuser = await UserModel.find(req.body).lean();
+  if (checkuser.length >= 1) {
+    let payload = {
+      ...checkuser[0],
+      token: 12345,
+    };
+    console.log(checkuser);
+    res.send(payload);
+  } else {
+    res.send({message: 'wrong credentials'});
+  }
 });
 
 module.exports = authRouter;
--- server/index.js
+++ server/index.js
@@ -1,43 +1,38 @@
-const express = require("express");
-const fs = require("fs");
-const mongodb = require("mongodb");
-const cors = require("cors")
-const  Team = require("./project/route/teamRoute.js")
+const express = require('express');
+const fs = require('fs');
+const mongodb = require('mongodb');
+const cors = require('cors');
+const Team = require('./project/route/teamRoute.js');
 const app = express();
-const clientRouter = require("./project/route/clientsRoute")
-const authRouter = require("./Routes/auth.routes");
-require("dotenv").config()
-const PORT = process.env.PORT || 4040
-const  UserModel  = require("./Models/User.auth")
-const {connection} = require("./project/database/teamMember");
-const { application } = require("express");
-const user = require("./Routes/user.js");
+const clientRouter = require('./project/route/clientsRoute');
+const authRouter = require('./Routes/auth.routes');
+require('dotenv').config();
+const PORT = process.env.PORT || 4040;
+const UserModel = require('./Models/User.auth');
+const {connection} = require('./project/database/teamMember');
+const {application} = require('express');
+const user = require('./Routes/user.js');
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
- app.use(cors())
+app.use(cors());
 //   app.use("/",clientRouter)
-  //  app.use("/users",user)
-    app.use("/",authRouter)
-  app.use("/client",clientRouter)
- app.use("/teamMember",Team)
-
-  
-     
-  
-      
-
-  
+//  app.use("/users",user)
+app.use('/', authRouter);
+app.use('/client', clientRouter);
+app.use('/teamMember', Team);
 
 //  app.use("/teamMemberdata",Team)
-
- app.listen(PORT, async () => {
- try {
-    await connection;
-    console.log("connection to db");
-  } catch {
-    console.log("failled");
-  }
-  console.log("server starting");
+app.get('/', (req, res) => {
+  res.send('Hello world');
 });
 
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log('connection to db');
+  } catch {
+    console.log('failled');
+  }
+  console.log('server starting');
+});
